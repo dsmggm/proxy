f1de5cdc535b38f83e5b8346c40582dadb0a185e
517939148yjf <141004278+517939148yjf@users.noreply.github.com>
2024-01-15 14:32:19 +0800

    Add files via upload

---
+++ xiequ.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+#本脚本需要requests依赖
+#本脚本仅适合用于携趣白名单的自动获取
+
+delall = '127.0.0.1'   #填写删除所有IP的连接，参考携趣官网白名单管理接口,例如  delall = 'http://op.xiequ.cn/IpWhiteList.aspx?uid=128590&ukey=945GGGGGE068AAD02A45B7059E8E380B&act=del&ip=all'
+iip = '127.0.0.1'    #填写添加白名单记录连接，参考携趣官网白名单管理接口,例如  iip = 'http://op.xiequ.cn/IpWhiteList.aspx?uid=128590&ukey=9G5EA120E067AAD02A45B7059E8E380B&act=add&ip='
+
+import requests
+
+def get_public_ip():
+    url = 'https://api.ipify.org?format=json'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        ip = data['ip']
+        return ip
+    else:
+        return None
+
+ip = get_public_ip()
+if ip:
+    print("当前公网IP地址是:", ip)
+else:
+    print("无法获取当前公网IP地址")
+
+# 打开删除所有IP网址
+response = requests.get("{delall}")
+
+# 拼接带有IP参数的网址
+url_with_ip = f"{iip}{ip}"
+
+# 打开带有IP参数的网址
+response_with_ip = requests.get(url_with_ip)
